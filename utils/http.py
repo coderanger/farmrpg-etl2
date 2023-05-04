211d0a0bbc767f750d342f18799018995b1f2d20
Noah Kantrowitz <noah@coderanger.net>
2023-05-04 01:26:50 -0700

    🎨 Try to fix  tests.

--- utils/http.py
+++ utils/http.py
@@ -14,9 +14,5 @@ def _client(cookie: str) -> httpx.AsyncClient:
     )
 
 
-client = _client(os.environ["AUTH_COOKIE"]) if os.environ.get("AUTH_COOKIE") else None
-bot_client = (
-    _client(os.environ["BOT_AUTH_COOKIE"])
-    if os.environ.get("BOT_AUTH_COOKIE")
-    else None
-)
+client = _client(os.environ.get("AUTH_COOKIE", ""))
+bot_client = _client(os.environ.get("BOT_AUTH_COOKIE", ""))
