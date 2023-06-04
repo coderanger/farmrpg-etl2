7f7de9f655d9191802eacf03c9ecdbb352ad28b5
Noah Kantrowitz <noah@coderanger.net>
2023-06-03 23:09:37 -0700

    ✨ Allow filtering to passwords that have clues.

--- passwords/graphql.py
+++ passwords/graphql.py
@@ -1,5 +1,6 @@
 from strawberry_django_plus import gql
 from strawberry_django_plus.gql import auto
+from django.db.models import QuerySet
 
 from items.graphql import Item
 
@@ -19,11 +20,21 @@ class PasswordGroup:
 
 @gql.django.filters.filter(models.Password)
 class PasswordFilter:
+    id: int
     password: auto
+    has_clues: bool
+
+    def filter_has_clues(
+        self, queryset: QuerySet[models.Password]
+    ) -> QuerySet[models.Password]:
+        if self.has_clues:
+            return queryset.exclude(clue1=None, clue2=None, clue3=None)
+        return queryset
 
 
 @gql.django.type(models.Password, filters=PasswordFilter)
 class Password:
+    id: int
     group: PasswordGroup
     password: auto
     clue1: auto
