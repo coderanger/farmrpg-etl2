a25b6a50f0b219ed178053d87b62da4c7c563d52
Noah Kantrowitz <noah@coderanger.net>
2023-06-09 23:03:23 -0700

    ✨ Filters and orders for NPCs.

--- npcs/graphql.py
+++ npcs/graphql.py
@@ -1,7 +1,7 @@
 from strawberry_django_plus import gql
 from strawberry_django_plus.gql import auto
 
-from items.graphql import Item
+from items.graphql import Item, ItemFilter, ItemOrder
 
 from . import models
 
@@ -13,7 +13,14 @@ class NPCFilter:
     short_name: auto
 
 
-@gql.django.type(models.NPC, filters=NPCFilter)
+@gql.django.ordering.order(models.NPC)
+class NPCOrder:
+    id: auto
+    name: auto
+    short_name: auto
+
+
+@gql.django.type(models.NPC, filters=NPCFilter, order=NPCOrder)
 class NPC:
     id: int
     name: auto
@@ -24,14 +31,42 @@ class NPC:
     npc_rewards: list["NPCReward"]
 
 
-@gql.django.type(models.NPCItem)
+@gql.django.filters.filter(models.NPCItem)
+class NPCItemFilter:
+    npc: NPCFilter
+    item: ItemFilter
+    relationship: auto
+
+
+@gql.django.ordering.order(models.NPCItem)
+class NPCItemOrder:
+    npc: NPCOrder
+    item: ItemOrder
+    relationship: auto
+
+
+@gql.django.type(models.NPCItem, filters=NPCItemFilter, order=NPCItemOrder)
 class NPCItem:
     npc: NPC
     item: Item
     relationship: auto
 
 
-@gql.django.type(models.NPCReward)
+@gql.django.filters.filter(models.NPCReward)
+class NPCRewardFilter:
+    npc: NPCFilter
+    item: ItemFilter
+
+
+@gql.django.ordering.order(models.NPCReward)
+class NPCRewardOrder:
+    npc: NPCOrder
+    item: ItemOrder
+    level: auto
+    order: auto
+
+
+@gql.django.type(models.NPCReward, filters=NPCRewardFilter, order=NPCRewardOrder)
 class NPCReward:
     npc: NPC
     level: auto
