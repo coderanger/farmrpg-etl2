ec5f11902714e8944c1900fe2196bdaf7bd6d320
Noah Kantrowitz <noah@coderanger.net>
2023-06-09 21:07:10 -0700

    ✨ Pretty render of password rewards.

--- passwords/admin.py
+++ passwords/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import format_html, mark_safe
 
 from .models import Password, PasswordGroup, PasswordItem
 
@@ -22,7 +23,7 @@ class PasswordItemInline(admin.TabularInline):
 
 @admin.register(Password)
 class PasswordAdmin(admin.ModelAdmin):
-    list_display = ["password", "group", "has_all_clues"]
+    list_display = ["password", "group", "has_all_clues", "reward"]
     list_editable = ["group"]
     readonly_fields = [
         "password",
@@ -34,3 +35,14 @@ class PasswordAdmin(admin.ModelAdmin):
     @admin.display(description="Has All Clues", boolean=True)
     def has_all_clues(self, password: Password) -> bool:
         return bool(password.clue1 and password.clue2 and password.clue3)
+
+    @admin.display(description="Reward")
+    def reward(self, password: Password) -> str:
+        parts = []
+        if password.reward_silver:
+            parts.append(format_html("Silver x{}", password.reward_silver))
+        if password.reward_gold:
+            parts.append(format_html("Gold x{}", password.reward_gold))
+        for item in password.reward_items.all().select_related("item"):
+            parts.append(format_html("{} x{}", item.item.name, item.quantity))
+        return mark_safe("<br>".join(parts))
