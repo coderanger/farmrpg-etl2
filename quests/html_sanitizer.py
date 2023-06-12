eeacddcfec681117e6687faec662d0c57ea2fe4c
Noah Kantrowitz <noah@coderanger.net>
2023-06-12 10:55:22 -0700

    ✨ Remove more than 2 BRs in a row.

--- quests/html_sanitizer.py
+++ quests/html_sanitizer.py
@@ -1,4 +1,5 @@
 from lxml import html
+from lxml.etree import _Element
 from lxml.html.clean import Cleaner
 
 
@@ -27,9 +28,17 @@ def _sanitize_style(style: str) -> str:
     return ";".join(":".join(r) for r in rules if r[0].strip() in SAFE_STYLES)
 
 
+def _is_br(elm: _Element | None, no_tail=True) -> bool:
+    return (
+        elm is not None
+        and elm.tag == "br"
+        and ((not no_tail) or elm.tail is None or not elm.tail.strip())
+    )
+
+
 def sanitize_quest_description(description: str) -> str:
     description = description.replace("{mycabbages}", "")
-    root = html.fragment_fromstring(description, create_parent=True)
+    root: _Element = html.fragment_fromstring(description, create_parent=True)
     CLEANER(root)
     for elm in root.iterdescendants():
         style = elm.get("style")
@@ -42,9 +51,16 @@ def sanitize_quest_description(description: str) -> str:
         # Remove any now-useless <fonts>.
         if elm.tag == "font" and not elm.attrib:
             elm.drop_tag()
+        # Remove more than two <br>s in a row.
+        if (
+            _is_br(elm)
+            and _is_br(elm.getnext())
+            and _is_br(elm.getnext().getnext(), no_tail=False)
+        ):
+            elm.drop_tag()
     # Strip trailing <br>s.
     for elm in reversed(root):
-        if elm.tag == "br" and (elm.tail is None or not elm.tail.strip()):
+        if _is_br(elm):
             elm.drop_tag()
         else:
             break
