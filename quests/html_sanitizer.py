d6cba31bca57b2f3f603d28b57d45c4b3f51c4f1
Noah Kantrowitz <noah@coderanger.net>
2023-06-12 00:37:04 -0700

    🔥 Remove the debug helper.

--- quests/html_sanitizer.py
+++ quests/html_sanitizer.py
@@ -1,11 +1,5 @@
-from collections import defaultdict
-
 from lxml import html
 from lxml.html.clean import Cleaner
-
-from utils.parsers import parse_page_fragment
-
-from .models import Quest
 
 
 SAFE_ATTRS = (html.defs.safe_attrs | {"style"}) - {
@@ -56,33 +50,3 @@ def sanitize_quest_description(description: str) -> str:
     inner_clean_content = "".join(html.tostring(e, encoding="unicode") for e in root)
     clean_content = f"{root.text or ' '}{inner_clean_content}"
     return clean_content
-
-
-def find_all_tags():
-    tags = set()
-    attrs = defaultdict(set)
-    css = defaultdict(set)
-    for q in Quest.objects.all():
-        root = parse_page_fragment(q.description)
-        for elm in root.iterdescendants():
-            if elm.tag not in tags:
-                print(q.id, q.title, "new tag", elm.tag)
-            tags.add(elm.tag)
-            for key in elm.attrib:
-                if key not in attrs[elm.tag]:
-                    print(q.id, q.title, "new attr", elm.tag, key)
-                attrs[elm.tag].add(key)
-            style = elm.get("style")
-            if style:
-                for rule in style.split(";"):
-                    spec = rule.split(":")[0].strip()
-                    if spec not in css[elm.tag]:
-                        print(q.id, q.title, "new style", elm.tag, spec)
-                    css[elm.tag].add(spec)
-    # print("attrs:")
-    # for key, val in sorted(attrs.items()):
-    #     print(f"{key}: {','.join(val)}")
-    # print("")
-    # print("css:")
-    # for key, val in sorted(css.items()):
-    #     print(f"{key}: {','.join(val)}")
