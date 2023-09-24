d7d8c0cf2ce71e65d98eeb98a73e9cc5480acf52
Noah Kantrowitz <noah@coderanger.net>
2023-09-23 19:11:49 -0700

    🐛 Forgot about hostkey management.

--- src/farmrpg_etl/ssh_tunnel/tunnel.py
+++ src/farmrpg_etl/ssh_tunnel/tunnel.py
@@ -19,12 +19,14 @@ _TUNNEL_READY = False
 
 async def _connect_one(server: Server):
     hostname, port = os.environ[f"SSH_{server.upper()}_HOST"].split(":")
+    hostkeys = os.environb[f"SSH_{server.upper()}_HOSTKEYS".encode()]
     key = asyncssh.import_private_key(os.environ["SSH_KEY"])
     conn = await asyncssh.connect(
         hostname,
         port=int(port),
         client_keys=[key],
         keepalive_interval=1,
+        known_hosts=hostkeys,
     )
     await conn.forward_local_port("127.0.0.1", FORWARD_PORTS[server], "127.0.0.1", 3306)
 
